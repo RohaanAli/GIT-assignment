45845f1d6e54909deaac8cbdd68ffd84035950a2
cygwins <chengyuge925@gmail.com>
2018-04-15 22:37:39 -0700

    fixed namespace in Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+using std::cout;
+using std::endl;
 int main()
 {
-	cout << "Hello Git";
-	cout << "Adding line";
+	cout << "Hello Git" << endl;
+	cout << "Adding line" << endl;
 }
